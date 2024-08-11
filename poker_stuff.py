0e4d14d0e47318705a6a2689ca6cf784be19b0ff
NoradBocaj <jmdoran19@yahoo.com>
2024-08-11 18:32:59 -0500

    umm

---
+++ poker_stuff.py
@@ -0,0 +1,31 @@
+class Hand:
+    def __init__(self):
+        self.pos: str = ""
+        self.bb_stakes: float = 0
+        self.profit_from_hand: float = 0
+        self.rake: float = 0
+        self.handNumber: int = 0
+        self.pfr: bool = False
+        self.VPip: bool = False
+        self.hero_hand: str = ""
+        self.villian_hand: str = ""
+    
+    def startHandAction(self):
+        self.handaction: HandAction = HandAction(self.bb_stakes)
+    
+
+class HandAction:
+    def __init__(self, bb_stakes):
+        self.bb_stakes: float = bb_stakes
+        self.preflop: str = ""
+        self.flop: str = ""
+        self.turn: str = ""
+        self.river: str = ""
+        self.profit_from_hand: float = 0
+
+
+class Session:
+    def __init__(self):
+        self.sessionHands: list[Hand] = []
+        self.date: str = ""
+        self.sessionProfit: float = 0
